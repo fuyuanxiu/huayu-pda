ad3d929173089aa650481e6e5b6cd159d4fcc813
Clelo-567 <18318912888@163.com>
2020-11-23 17:19:53 +0800

    更替app图标，文本页面修改

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 var API = window.API = {
 	//webPath: "http://192.168.0.105:8082/",//test
-	webPath: "http://192.168.1.160:9997/",
-	//webPath: "http://192.168.0.21:8081/",//offical
+	//webPath: "http://192.168.1.160:9997/",
+	webPath: "http://192.168.0.21:8081/",//offical
 	version: "test", //demo:静态，prod:正式，test:测试
 	urls: {
 		login: "anon/login!login.action",
